a1ad6ae5ab3f03112f7e6e5e73cf427e5bb4fb49
sharkwyf <122104004@qq.com>
2021-12-08 19:58:03 +0800

    Initial Submit

---
+++ ResourceSchedulingProject/cppFiles/ResourceScheduler.cpp
@@ -0,0 +1,133 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "../hFiles/ResourceScheduler.h"
+
+
+ResourceScheduler::ResourceScheduler(int tasktype,int caseID) {
+	taskType = tasktype;
+	string filePath = "../input/task" + to_string(taskType) + "_case"+to_string(caseID)+".txt";
+	freopen(filePath.c_str(), "r", stdin);
+	cin >> numJob >> numHost >> alpha;
+	if (taskType == 2)
+		cin >> St;
+	hostCore.resize(numHost);
+	for (int i = 0; i < numHost; i++)
+		cin >> hostCore[i];
+
+	jobBlock.resize(numJob);
+	for (int i = 0; i < numJob; i++)
+		cin >> jobBlock[i];
+
+	Sc.resize(numJob);
+	for (int i = 0; i < numJob; i++)
+		cin >> Sc[i];
+
+	dataSize.resize(numJob);
+	for (int i = 0; i < numJob; i++) {
+		dataSize[i].resize(jobBlock[i]);
+		for (int j = 0; j < jobBlock[i]; j++)
+			cin >> dataSize[i][j];
+	}
+
+	location.resize(numJob);
+	for (int i = 0; i < numJob; i++) {
+		location[i].resize(jobBlock[i]);
+		for (int j = 0; j < jobBlock[i]; j++)
+			cin >> location[i][j];
+	}
+
+	jobFinishTime.resize(numJob, 0);
+	jobCore.resize(numJob);
+
+	runLoc.resize(numJob);
+	for (int i = 0; i < numJob; i++)
+		runLoc[i].resize(jobBlock[i]);
+
+	hostCoreTask.resize(numHost);
+	for (int i = 0; i < numHost; i++)
+		hostCoreTask[i].resize(hostCore[i]);
+
+	hostCoreFinishTime.resize(numHost);
+	for (int i = 0; i < numHost; i++)
+		hostCoreFinishTime[i].resize(hostCore[i], 0);
+}
+
+void ResourceScheduler::schedule() {
+
+	vector<vector<int>> hostCoreBlock(numHost);
+	for (int i = 0; i < numHost; i++)
+		hostCoreBlock[i].resize(hostCore[i], 0);
+
+	for (int i = 0; i < numJob; i++) {
+		set<pair<int, int>> allocatedJobCore;
+		for (int j = 0; j < jobBlock[i]; j++) {
+			int hid = rand() % numHost;
+			int cid = rand() % hostCore[hid];
+			allocatedJobCore.insert({ hid,cid });
+			runLoc[i][j] = make_tuple(hid, cid, hostCoreBlock[hid][cid]++);
+		}
+		/*for (int j = 0; j < jobBlock[i]; j++)
+			finishTime[i]+=*/
+		jobFinishTime[i] = rand() % 200;
+		jobCore[i] = allocatedJobCore.size();
+	}
+
+
+	for (int i = 0; i < numHost; i++) {
+		for (int j = 0; j < hostCore[i]; j++) {
+			int numTask = rand() % 10 + 1;
+			hostCoreTask[i][j].resize(numTask);
+			for (int k = 0; k < numTask; k++) {
+				int jid = rand() % numJob;
+				int bid = rand() % jobBlock[jid];
+				int endTime = hostCoreFinishTime[i][j] + rand() % 100 + 1;
+				hostCoreTask[i][j][k] = make_tuple(jid, bid, hostCoreFinishTime[i][j], endTime);
+				hostCoreFinishTime[i][j] = endTime;
+			}
+		}
+	}
+}
+
+void ResourceScheduler::outputSolutionFromBlock() {
+	cout << "\nTask2 Solution (Block Perspective) of Teaching Assistant:\n\n";
+	for (int i = 0; i < numJob; i++) {
+		double speed = g(jobCore[i]);
+		cout << "Job" << i << " obtains " << jobCore[i] << " cores (speed=" << speed << ") and finishes at time " << jobFinishTime[i] << ": \n";
+		for (int j = 0; j < jobBlock[i]; j++) {
+			cout << "\tBlock" << j << ": H" << get<0>(runLoc[i][j]) << ", C" << get<1>(runLoc[i][j]) << ", R" << get<2>(runLoc[i][j]) << " (time=" << fixed << setprecision(2) << dataSize[i][j] / speed << ")" << " \n";
+		}
+		cout << "\n";
+	}
+
+	cout << "The maximum finish time: " << *max_element(jobFinishTime.begin(), jobFinishTime.end()) << "\n";
+	cout << "The total response time: " << accumulate(jobFinishTime.begin(), jobFinishTime.end(), 0.0) << "\n\n";
+}
+
+void ResourceScheduler::outputSolutionFromCore() {
+	cout << "\nTask2 Solution (Core Perspective) of Teaching Assistant:\n\n";
+	double maxHostTime = 0, totalRunningTime = 0.0;
+	for (int i = 0; i < numHost; i++) {
+		double hostTime = *max_element(hostCoreFinishTime[i].begin(), hostCoreFinishTime[i].end());
+		maxHostTime = max(hostTime, maxHostTime);
+		totalRunningTime += accumulate(hostCoreFinishTime[i].begin(), hostCoreFinishTime[i].end(), 0.0);
+		cout << "Host" << i << " finishes at time " << hostTime << ":\n\n";
+		for (int j = 0; j < hostCore[i]; j++) {
+			cout << "\tCore" << j << " has " << hostCoreTask[i][j].size() << " tasks and finishes at time " << hostCoreFinishTime[i][j] << ":\n";
+			for (int k = 0; k < hostCoreTask[i][j].size(); k++) {
+				cout << "\t\tJ" << setw(2) << setfill('0') << get<0>(hostCoreTask[i][j][k]) << ", B" << setw(2) << setfill('0') << get<1>(hostCoreTask[i][j][k]) << ", runTime " << fixed << setprecision(1) << setw(5) << setfill('0') << get<2>(hostCoreTask[i][j][k]) << " to " << fixed << setprecision(1) << setw(5) << setfill('0') << get<3>(hostCoreTask[i][j][k]) << "\n";
+			}
+			cout << "\n";
+		}
+		cout << "\n\n";
+	}
+	cout << "The maximum finish time of hosts: " << maxHostTime << "\n";
+	cout << "The total efficacious running time: " << totalRunningTime << "\n";
+	cout << "Utilization rate: " << totalRunningTime / accumulate(hostCore.begin(), hostCore.end(), 0.0) / maxHostTime << "\n\n";
+}
+
+void ResourceScheduler::visualization() {
+
+}
+
+double ResourceScheduler::g(int e) {
+	return 1 - alpha * (e - 1);
+}
